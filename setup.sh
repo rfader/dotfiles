3bd8207dfa00e91155b973853885a3293f3bcd3a
Rasmus Faddersbøll <rasmus.faddersboell@gmail.com>
2017-02-16 13:49:14 +0100

    Adds homebrew install to setup script

--- setup.sh
+++ setup.sh
@@ -7,8 +7,12 @@ os_name="$(uname -s)"
 if [ "$os_name" == "Darwin" ]; then
   echo "Run macOS setup"
 
+  # Install Homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
   # Install all google fonts
   curl https://raw.githubusercontent.com/qrpike/Web-Font-Load/master/install.sh | sh
+
   # sh .macos
 fi
 
