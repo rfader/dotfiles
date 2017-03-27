6c35889d1ee146a0ffcd59eb10831d3424c68e99
Rasmus Faddersbøll <rasmus.faddersboell@gmail.com>
2017-03-27 07:08:37 +0200

    Installs git on get

--- get.sh
+++ get.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -eux
 
+# install git
+os_name="$(uname -s)"
+if [ "$os_name" == "Linux" ]; then
+  # debian & ubuntu
+  which apt-get && sudo apt-get update && sudo apt-get install git
+fi
+
+
+mkdir ~/.dotfiles
 git clone https://github.com/rfader/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 # ./update.sh
-
